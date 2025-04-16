28c8e7652c40d9461814db531e6879871a375039
Nannu9057 <89435711+Nannu9057@users.noreply.github.com>
2025-04-16 16:09:56 +0530

    Add files via upload

---
+++ animeData.js
@@ -0,0 +1,18 @@
+
+const animeData = [
+  {
+    title: "Attack on Titan",
+    thumbnail: "https://via.placeholder.com/200x300?text=AOT",
+    episodes: [
+      { video: "https://www.w3schools.com/html/mov_bbb.mp4" },
+      { video: "https://www.w3schools.com/html/movie.mp4" }
+    ]
+  },
+  {
+    title: "Naruto",
+    thumbnail: "https://via.placeholder.com/200x300?text=Naruto",
+    episodes: [
+      { video: "https://www.w3schools.com/html/mov_bbb.mp4" }
+    ]
+  }
+];
